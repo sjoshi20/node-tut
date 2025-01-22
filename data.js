b47cc65111589756e35071bb03659f4c3a87c5e5
sjoshi20 <joshishivani426@gmail.com>
2025-01-22 18:04:25 +0530

    Data file

---
+++ data.js
@@ -0,0 +1,8 @@
+
+const data = [
+
+{name:'Seeta',email:'seeta@test.com',contact:'9999'},
+{name:'Geeta',email:'geeta@test.com',contact:'8888'},
+{name:'Meera',email:'meera@test.com',contact:'7777'}
+]
+export default data;
--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-import chalk from 'chalk';
-console.log(chalk.blue('Hello world!'));
+import http from 'http';
+import data from './data.js';
+http.createServer((req,resp) => {
+   resp.writeHead(200,{'Content-Type':'application\json'});
+   resp.write(JSON.stringify(data));
+   resp.end();
+}).listen(5000);
